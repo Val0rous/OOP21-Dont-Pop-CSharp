87b55b83c913403cf4249f04bcb0ea56f419c597
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-29 16:19:56 +0200

    create common solution

---
+++ Francesco-Valentini/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Francesco_Valentini
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Mario-Biavati/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mario_Biavati
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
