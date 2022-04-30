12aec8809681ab3401c1820ecaa6f23558fb8617
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2022-04-30 10:43:56 +0200

    add test class for Point2D

--- Michele-Ravaioli/UnitTest1.cs
+++ Michele-Ravaioli/UnitTest1.cs
@@ -7,7 +7,21 @@ namespace Michele_Ravaioli
         [Test]
         public void TestPoint2D()
         {
+            var a = new Point2D(3, 3);
+            var b = Point2D.Of(2, 2);
+            double f = 10;
 
+            Assert.IsFalse(a == b);
+            Assert.IsFalse(a * f == a);
+            Assert.IsTrue(b * f != b);
+
+            var c = a + b;
+
+            Assert.IsTrue(c.X == 5);
+
+            a.Y += 1;
+
+            Assert.IsTrue(a.Magnitude == 5);
         }
     }
 }
