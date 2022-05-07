ad3ff5a6038bb7fbf4b1d46c28dc65faf23d30db
Roboardo <mario.biavati@studio.unibo.it>
2022-05-07 12:51:23 +0200

    Added PlayerObj and related tests

---
+++ Mario-Biavati/PlayerObj.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MarioBiavati
+{
+    class PlayerObj
+    {
+        public Point2D position;
+        Point2D movement;
+        double speed = 0.075;
+
+        public PlayerObj(Point2D pos, double s)
+        {
+            position = pos;
+            speed = s;
+        }
+
+        public void Update(Point2D mousePosition)
+        {
+            movement = mousePosition - position;
+            if (movement.Magnitude <= speed) this.position += movement;
+            else this.position += (movement / movement.Magnitude) * speed;
+        }
+    }
+}
--- Mario-Biavati/UnitTest1.cs
+++ Mario-Biavati/UnitTest1.cs
@@ -1,13 +1,30 @@
 using NUnit.Framework;
 
-namespace Michele_Ravaioli
+namespace MarioBiavati
 {
     public class UnitTest1
     {
         [Test]
-        public void TestPoint2D()
+        public void TestPlayer()
         {
+            var speed = 1;
+            var player = new PlayerObj(new Point2D(0, 0), speed);
+            var mousePos1 = new Point2D(0, 0);
+            var mousePos2 = new Point2D(0.5, 0.5);
+            var mousePos3 = new Point2D(10.5, 10.5);
 
+            player.Update(mousePos1);
+
+            Assert.IsTrue(player.position == mousePos1);
+
+            player.Update(mousePos2);
+
+            Assert.IsTrue(player.position == mousePos2);
+
+            player.Update(mousePos3);
+
+            Assert.IsFalse(player.position == mousePos3);
+            Assert.IsTrue(player.position == mousePos2 + ((mousePos3 / mousePos3.Magnitude) * speed));
         }
     }
 }
